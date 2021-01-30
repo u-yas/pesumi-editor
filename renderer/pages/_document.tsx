8e2f680aa25653a69cc5bdf3aea228f29f40f3f7
yuya <yuya3.21998@gmail.com>
2021-01-30 09:10:38 +0900

    meta viewportを削除

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <Html>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
         <body>
           <Main />
