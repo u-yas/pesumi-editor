1cfbd0ed2aa6421de939eb31d158f773b18e0add
yuya <yuya3.21998@gmail.com>
2021-02-12 06:20:00 +0900

    update

--- main/helpers/ipc/openStandingCharacterImage.ts
+++ main/helpers/ipc/openStandingCharacterImage.ts
@@ -1,9 +1,10 @@
 import { dialog } from 'electron'
 import fs from 'fs'
 /**
- * キャラクターの立ち絵をプロジェクトフォルダにインポートする処理
+ * キャラクターの立ち絵の画像ファイルやBGMの音声ファイル等をプロジェクトフォルダにインポートする処理
  *  @param Character どのキャラクターの画像なのかを選択する
  *  @param mainWindow メインウインドウ
+ *  @param fileType インポートするファイルの種類、例えば、背景画像なのか、キャラクターの立ち絵なのか、EventCGなのか
  */
 const openStandingCharacterImage = (name:string, fileType:string, mainWindow:Electron.BrowserWindow):Promise<string> => {
   return new Promise((resolve, reject) => {
@@ -14,7 +15,7 @@ const openStandingCharacterImage = (name:string, fileType:string, mainWindow:Ele
         'openFile'
       ],
       filters: [
-
+        // 音声ファイル、画像ファイルのうちのどれか
       ]
     }).then(returnValue => {
       // ファイルパスを所得したのでそのパスのファイルをプロジェクトフォルダの所定の位置にコピーする
