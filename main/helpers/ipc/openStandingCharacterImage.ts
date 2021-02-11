ff74fbb8d8f4a5f247f02844b310e210eb556a23
yuya <yuya3.21998@gmail.com>
2021-02-11 21:58:42 +0900

    キャラクターの立ち絵を保存する処理

---
+++ main/helpers/ipc/openStandingCharacterImage.ts
@@ -0,0 +1,14 @@
+import { rejects } from 'assert'
+
+/**
+ * キャラクターの立ち絵をプロジェクトフォルダにインポートする処理
+ *  @param Character どのキャラクターの画像なのかを選択する
+ */
+const openStandingCharacterImage = () => {
+  return new Promise((resolve, reject) => {
+    // reject({})
+    resolve({})
+  })
+}
+
+export default openStandingCharacterImage
